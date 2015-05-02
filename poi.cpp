4563519d25ce66f758813e1c8029595265c5752b
mfikria <mfikria@students.itb.ac.id>
2015-05-02 18:59:32 +0700

    Update poi.cpp

--- poi.cpp
+++ poi.cpp
@@ -17,7 +17,23 @@ void POIFS::createPoi(const char *filename)
 
 void POIFS::initVolumeInformation() /* Define the newly created .poi allocation table (which refer to data pool) */
 {
+	char buffer[BLOCK_SIZE]; // buffer untuk menyimpan Volume Information
 	
+	memset(buffer, 0, BLOCK_SIZE);
+	
+	memcpy(buffer + 0x00, "poi!", 4); // Validasi bahwa file yang dibaca adalah file .poi yang valid.
+	
+	memcpy(buffer + 0x04, mountname, strlen(mountname)); // Sebuah null-terminated string yang menyimpan nama dari volume. 
+	
+	memcpy(buffer + 0x24, (char*)&DATAPOOL_BLOCK_N, 4); // Kapasitas filesystem dalam blok, ditulis dalam integer 32-bit little endian. 
+
+	memcpy(buffer + 0x28, (char*)&(DATAPOOL_BLOCK_N - 1), 4); // Jumlah blok yang belum terpakai, ditulis dalam integer 32bit little endian. 
+
+	memcpy(buffer + 0x2C, (char*)&1, 4);
+	
+	memcpy(buffer + 0x1FC, "!iop", 4);
+	
+	handle.write(buffer, BLOCK_SIZE);
 }
 void POIFS::initAllocTable() /* Define the newly created .poi data pool */
 {
