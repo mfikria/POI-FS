e6af73b948a96d5f08294866bbe0588e6ffa03e4
mfikria <mfikria@students.itb.ac.id>
2015-05-02 20:49:12 +0700

    Update poi.cpp

--- poi.cpp
+++ poi.cpp
@@ -1,6 +1,7 @@
 #include "poi.hpp"
 POIFS::POIFS()
 {
+	time(&mount_time);
 	
 }
 
@@ -10,6 +11,9 @@ POIFS::~POIFS(){
 void POIFS::createPoi(const char *filename)
 {
 	mountname = (string) filename;
+	blockCapacity = DATAPOOL_BLOCK_N;
+	availBlock = blockCapacity - 1;
+	firstAvail = 1;
 	target.open(filename, fstream::in | fstream::out | fstream::binary | fstream::trunc);
 	printf("Writing to %s\n", filename);
 	initVolumeInformation();
@@ -25,23 +29,35 @@ void POIFS::initVolumeInformation() /* Define the newly created .poi allocation 
 	
 	memcpy(buffer + 0x04, mountname, strlen(mountname)); // Sebuah null-terminated string yang menyimpan nama dari volume. 
 	
-	memcpy(buffer + 0x24, (char*)&DATAPOOL_BLOCK_N, 4); // Kapasitas filesystem dalam blok, ditulis dalam integer 32-bit little endian. 
+	memcpy(buffer + 0x24, (char*)&blockCapacity, 4); // Kapasitas filesystem dalam blok, ditulis dalam integer 32-bit little endian. 
 
-	memcpy(buffer + 0x28, (char*)&(DATAPOOL_BLOCK_N - 1), 4); // Jumlah blok yang belum terpakai, ditulis dalam integer 32bit little endian. 
+	memcpy(buffer + 0x28, (char*)&availBlock, 4); // Jumlah blok yang belum terpakai, ditulis dalam integer 32bit little endian. 
 
-	memcpy(buffer + 0x2C, (char*)&1, 4);
+	memcpy(buffer + 0x2C, (char*)&firstAvail, 4); // Indeks blok pertama yang bebas, ditulis dalam integer 32-bit little endian.  
 	
-	memcpy(buffer + 0x1FC, "!iop", 4);
+	memcpy(buffer + 0x1FC, "!iop", 4); // String “!iop”
 	
-	handle.write(buffer, BLOCK_SIZE);
+	target.write(buffer, BLOCK_SIZE);
 }
 void POIFS::initAllocTable() /* Define the newly created .poi data pool */
 {
+	short buffer = 0xFFFF;
 	
+	handle.write((char*)&buffer, sizeof(short));
+	
+	buffer = 0;
+	for (int i = firstAvail; i < DATAPOOL_BLOCK_N; i++) {
+		target.write((char*)&buffer, sizeof(short));
+	}
 }
 void POIFS::initDataPool()
 {
+	char buffer[BLOCK_SIZE];
+	memset(buffer, 0, BLOCK_SIZE);
 	
+	for (int i = 0; i < DATAPOOL_BLOCK_N; i++) {
+		target.write(buffer, BLOCK_SIZE);
+	}
 }
 
 
