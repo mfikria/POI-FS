e1cab5ec2f9f9760593b06962cab238bfd6496e8
mfikria <mfikria@students.itb.ac.id>
2015-05-02 16:23:34 +0700

    test

--- poi.cpp
+++ poi.cpp
@@ -5,17 +5,23 @@ POIFS::POIFS()
   int iterator;
 	int it;
 
-	FILE *filesystem = fopen(filesystem_name, "w");
-
-	printf("Writing to %s\n", filesystem_name);
-
+	FILE *filesystem = fopen(filename, "w");
+	printf("Writing to %s\n", filename);
 	fprintf(filesystem, "poi!"); // Magic string “poi!” (tanpa kutipan), digunakan untuk validasi bahwa file yang dibaca adalah file .poi yang valid. 
 	for (iterator = 0; iterator < 32; ++iterator) // Sebuah null-terminated string yang menyimpan nama dari volume.
-			fprintf(filesystem, "%c", 0);
+		fprintf(filesystem, "%c", 0);
 	fprintf(filesystem, "%c%c%c%c", 0x00, 0x00, 0x01, 0x00); // Kapasitas filesystem dalam blok, ditulis dalam integer 32-bit little endian.
 	fprintf(filesystem, "%c%c%c%c", 0xFF, 0xFF, 0x00, 0x00); // Jumlah blok yang belum terpakai, ditulis dalam integer 32-bit little endian.
 	fprintf(filesystem, "%c%c%c%c", 0x01, 0x00, 0x00, 0x00); // Indeks blok pertama yang bebas, ditulis dalam integer 32-bit little endian.
+	// Nama file atau direktori.
+	fprintf(filesystem, "%c", 0x01);// Atribut file atau direktori.
+	fprintf(filesystem, "%c%c", 0x01, 0x00);// Waktu pembuatan atau modifikasi terakhir.
+	fprintf(filesystem, "%c%c", 0x01, 0x00);// Tanggal pembuatan atau modifikasi terakhir.
+	fprintf(filesystem, "%c%c", 0x01, 0x00);// Indeks blok data pool pertama penyimpanan file / direktori dalam 16-bit little endian.
+	fprintf(filesystem, "%c%c%c%c", 0x01, 0x00, 0x00, 0x00);// Ukuran file dalam byte, ditulis dalam integer 32-bit little endian.
 	for (iterator = 0; iterator < 428; ++iterator) // Kosong, diisi dengan nilai ‘\0’ (null character) 
 		fprintf(filesystem, "%c", 0x00);
-	fprintf(filesystem, "!iop");  
+	fprintf(filesystem, "!iop");
+	
+	
 }
