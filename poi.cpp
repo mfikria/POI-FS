a1827abdd6974585063497aad73976b5e81940f5
mfikria <mfikria@students.itb.ac.id>
2015-05-02 16:11:41 +0700

    Update poi.cpp

--- poi.cpp
+++ poi.cpp
@@ -1,1 +1,21 @@
 #include "poi.hpp"
+
+POIFS::POIFS()
+{
+  int iterator;
+	int it;
+
+	FILE *filesystem = fopen(filesystem_name, "w");
+
+	printf("Writing to %s\n", filesystem_name);
+
+	fprintf(filesystem, "poi!"); // Magic string “poi!” (tanpa kutipan), digunakan untuk validasi bahwa file yang dibaca adalah file .poi yang valid. 
+	for (iterator = 0; iterator < 32; ++iterator) // Sebuah null-terminated string yang menyimpan nama dari volume.
+			fprintf(filesystem, "%c", 0);
+	fprintf(filesystem, "%c%c%c%c", 0x00, 0x00, 0x01, 0x00); // Kapasitas filesystem dalam blok, ditulis dalam integer 32-bit little endian.
+	fprintf(filesystem, "%c%c%c%c", 0xFF, 0xFF, 0x00, 0x00); // Jumlah blok yang belum terpakai, ditulis dalam integer 32-bit little endian.
+	fprintf(filesystem, "%c%c%c%c", 0x01, 0x00, 0x00, 0x00); // Indeks blok pertama yang bebas, ditulis dalam integer 32-bit little endian.
+	for (iterator = 0; iterator < 428; ++iterator) // Kosong, diisi dengan nilai ‘\0’ (null character) 
+		fprintf(filesystem, "%c", 0x00);
+	fprintf(filesystem, "!iop");  
+}
